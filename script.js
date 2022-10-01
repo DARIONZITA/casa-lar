6c72e90cff1b7080b7b158c8f891fbdb7acef9c6
DARIONZITA <92880647+DARIONZITA@users.noreply.github.com>
2022-10-01 18:21:21 +0100

    upya

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 
-let ele = document.getElementById("works");
-window.addEventListener('scroll',()=>{console.log(window.innerHeight)})
-
+let works = document.getElementById("works");
+let number= document.querySelectorAll('.num');
+let obv = new IntersectionObserver( entres =>{
+    if(entres[0].isIntersecting===true){
+        number.forEach((v,k,a) => {
+            setInterval(()=>{
+            
+            },3000)
+        })
+       
+    }
+},{threshold : [0.50]});
+obv.observe(works)
 
