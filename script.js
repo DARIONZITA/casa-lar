1ca06c3ea880f5d37e93c42009f4357d4aacef87
DARIONZITA <92880647+DARIONZITA@users.noreply.github.com>
2022-10-01 20:08:59 +0100

    letal

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-
 let works = document.getElementById("works");
-let number= document.querySelectorAll('.num');
+let num= document.querySelectorAll('.num');
 let obv = new IntersectionObserver( entres =>{
     if(entres[0].isIntersecting===true){
-        number.forEach((v,k,a) => {
-            setInterval(()=>{
-            
-            },3000)
+        num.forEach((v,k,a) => {
+            let i = 0
+            const timen = setInterval(()=>{
+            v.innerHTML=`+ ${i++}`
+            switch(k){
+                case 0:
+                    if(i > 136){clearInterval(timen)}
+                    break
+                case 1:
+                    if(i > 95){clearInterval(timen)}
+                    break
+                case 2:
+                    if(i > 83){clearInterval(timen)}
+                    break
+                case 3:
+                    if(i > 105){clearInterval(timen)}
+                    break
+            }
+            },30);
+
         })
-       
+      
     }
 },{threshold : [0.50]});
 obv.observe(works)
